620713b8247baddd1f91adc316ce7e810984fa4a
PoonKinWang <PoonKinWang@outlook.com>
2021-04-24 03:01:58 +0800

    update

--- DistributedBatchNorm.py
+++ DistributedBatchNorm.py
@@ -8,12 +8,94 @@ from torch.nn import init
 from typing import Optional, Any
 
 class DistributedBatchNorm2d():
-    r"""BatchNorm2d Generator
-    vt_world_size: virtual_world_size"""
+    r"""
+    BatchNorm2d Generator. 
+    Args:
+        vt_world_size: virtual_world_size
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, H, W)`
+        eps: a value added to the denominator for numerical stability.
+            Default: 1e-5
+        momentum: the value used for the running_mean and running_var
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics, and initializes statistics
+            buffers :attr:`running_mean` and :attr:`running_var` as ``None``.
+            When these buffers are ``None``, this module always uses batch statistics.
+            in both training and eval modes. Default: ``True``
+    """
     def __init__(self, vt_world_size):
-        
         self.vt_world_size = vt_world_size
     def __call__(self,  **kwargs):
+        r"""Applies Batch Normalization over a 4D input (a mini-batch of 2D inputs
+            with additional channel dimension) as described in the paper
+            `Batch Normalization: Accelerating Deep Network Training by Reducing
+            Internal Covariate Shift <https://arxiv.org/abs/1502.03167>`__ .
+
+            .. math::
+
+                y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta
+
+            The mean and standard-deviation are calculated per-dimension over
+            the mini-batches and :math:`\gamma` and :math:`\beta` are learnable parameter vectors
+            of size `C` (where `C` is the input size). By default, the elements of :math:`\gamma` are set
+            to 1 and the elements of :math:`\beta` are set to 0. The standard-deviation is calculated
+            via the biased estimator, equivalent to `torch.var(input, unbiased=False)`.
+
+            Also by default, during training this layer keeps running estimates of its
+            computed mean and variance, which are then used for normalization during
+            evaluation. The running estimates are kept with a default :attr:`momentum`
+            of 0.1.
+
+            If :attr:`track_running_stats` is set to ``False``, this layer then does not
+            keep running estimates, and batch statistics are instead used during
+            evaluation time as well.
+
+            .. note::
+                This :attr:`momentum` argument is different from one used in optimizer
+                classes and the conventional notion of momentum. Mathematically, the
+                update rule for running statistics here is
+                :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momentum} \times x_t`,
+                where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
+                new observed value.
+
+            Because the Batch Normalization is done over the `C` dimension, computing statistics
+            on `(N, H, W)` slices, it's common terminology to call this Spatial Batch Normalization.
+
+            Args:
+                num_features: :math:`C` from an expected input of size
+                    :math:`(N, C, H, W)`
+                eps: a value added to the denominator for numerical stability.
+                    Default: 1e-5
+                momentum: the value used for the running_mean and running_var
+                    computation. Can be set to ``None`` for cumulative moving average
+                    (i.e. simple average). Default: 0.1
+                affine: a boolean value that when set to ``True``, this module has
+                    learnable affine parameters. Default: ``True``
+                track_running_stats: a boolean value that when set to ``True``, this
+                    module tracks the running mean and variance, and when set to ``False``,
+                    this module does not track such statistics, and initializes statistics
+                    buffers :attr:`running_mean` and :attr:`running_var` as ``None``.
+                    When these buffers are ``None``, this module always uses batch statistics.
+                    in both training and eval modes. Default: ``True``
+
+            Shape:
+                - Input: :math:`(N, C, H, W)`
+                - Output: :math:`(N, C, H, W)` (same shape as input)
+
+            Examples::
+
+                >>> # With Learnable Parameters
+                >>> m = nn.BatchNorm2d(100)
+                >>> # Without Learnable Parameters
+                >>> m = nn.BatchNorm2d(100, affine=False)
+                >>> input = torch.randn(20, 100, 35, 45)
+                >>> output = m(input)
+        """
         return BatchNorm2d(vt_world_size=self.vt_world_size, **kwargs)
 
 class _NormBase(Module):
@@ -98,7 +180,7 @@ class _BatchNorm(_NormBase):
 
     def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
                  track_running_stats=True, vt_world_size=1):
-    """vt_world_size: virtual_world_size"""
+        """vt_world_size: virtual_world_size"""
         if (type(vt_world_size) is int and vt_world_size > 0):
             print("virtual world size: "+str(vt_world_size))
         else:
@@ -237,72 +319,6 @@ class BatchNorm1d(_BatchNorm):
 
 
 class BatchNorm2d(_BatchNorm):
-    r"""Applies Batch Normalization over a 4D input (a mini-batch of 2D inputs
-    with additional channel dimension) as described in the paper
-    `Batch Normalization: Accelerating Deep Network Training by Reducing
-    Internal Covariate Shift <https://arxiv.org/abs/1502.03167>`__ .
-
-    .. math::
-
-        y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta
-
-    The mean and standard-deviation are calculated per-dimension over
-    the mini-batches and :math:`\gamma` and :math:`\beta` are learnable parameter vectors
-    of size `C` (where `C` is the input size). By default, the elements of :math:`\gamma` are set
-    to 1 and the elements of :math:`\beta` are set to 0. The standard-deviation is calculated
-    via the biased estimator, equivalent to `torch.var(input, unbiased=False)`.
-
-    Also by default, during training this layer keeps running estimates of its
-    computed mean and variance, which are then used for normalization during
-    evaluation. The running estimates are kept with a default :attr:`momentum`
-    of 0.1.
-
-    If :attr:`track_running_stats` is set to ``False``, this layer then does not
-    keep running estimates, and batch statistics are instead used during
-    evaluation time as well.
-
-    .. note::
-        This :attr:`momentum` argument is different from one used in optimizer
-        classes and the conventional notion of momentum. Mathematically, the
-        update rule for running statistics here is
-        :math:`\hat{x}_\text{new} = (1 - \text{momentum}) \times \hat{x} + \text{momentum} \times x_t`,
-        where :math:`\hat{x}` is the estimated statistic and :math:`x_t` is the
-        new observed value.
-
-    Because the Batch Normalization is done over the `C` dimension, computing statistics
-    on `(N, H, W)` slices, it's common terminology to call this Spatial Batch Normalization.
-
-    Args:
-        num_features: :math:`C` from an expected input of size
-            :math:`(N, C, H, W)`
-        eps: a value added to the denominator for numerical stability.
-            Default: 1e-5
-        momentum: the value used for the running_mean and running_var
-            computation. Can be set to ``None`` for cumulative moving average
-            (i.e. simple average). Default: 0.1
-        affine: a boolean value that when set to ``True``, this module has
-            learnable affine parameters. Default: ``True``
-        track_running_stats: a boolean value that when set to ``True``, this
-            module tracks the running mean and variance, and when set to ``False``,
-            this module does not track such statistics, and initializes statistics
-            buffers :attr:`running_mean` and :attr:`running_var` as ``None``.
-            When these buffers are ``None``, this module always uses batch statistics.
-            in both training and eval modes. Default: ``True``
-
-    Shape:
-        - Input: :math:`(N, C, H, W)`
-        - Output: :math:`(N, C, H, W)` (same shape as input)
-
-    Examples::
-
-        >>> # With Learnable Parameters
-        >>> m = nn.BatchNorm2d(100)
-        >>> # Without Learnable Parameters
-        >>> m = nn.BatchNorm2d(100, affine=False)
-        >>> input = torch.randn(20, 100, 35, 45)
-        >>> output = m(input)
-    """
-
     def _check_input_dim(self, input):
         if input.dim() != 4:
             raise ValueError('expected 4D input (got {}D input)'
