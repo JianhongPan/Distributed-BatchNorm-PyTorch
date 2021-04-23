09b471d24bf01e8c84a252c50cbd556d52ea1898
PoonKinWang <PoonKinWang@outlook.com>
2021-04-24 04:01:00 +0800

    update

--- DistributedBatchNorm.py
+++ DistributedBatchNorm.py
@@ -342,7 +342,7 @@ class _BatchNorm(_NormBase):
         output = []
         # vt_batch_size = ceil(input.shape[0] / self.world_size)
         vt_batch_size = (input.shape[0] + self.vt_world_size - 1) // self.vt_world_size
-        for gpu_index in range(self.vt_world_size):
+        for vt_gpu_index in range(self.vt_world_size):
             # exponential_average_factor is set to self.momentum
             # (when it is available) only so that it gets updated
             # in ONNX graph when this node is exported to ONNX.
@@ -354,7 +354,7 @@ class _BatchNorm(_NormBase):
             if self.training and self.track_running_stats:
                 # TODO: if statement only here to tell the jit to skip emitting this when it is None
                 if self.num_batches_tracked is not None:
-                    if gpu_index == 0:
+                    if vt_gpu_index == 0:
                         self.num_batches_tracked = self.num_batches_tracked + 1
                     if self.momentum is None:  # use cumulative moving average
                         exponential_average_factor = 1.0 / float(self.num_batches_tracked)
@@ -375,18 +375,19 @@ class _BatchNorm(_NormBase):
             passed when the update should occur (i.e. in training mode when they are tracked), or when buffer stats are
             used for normalization (i.e. in eval mode when buffers are not None).
             """
-            if gpu_index == 0:
+            if vt_gpu_index == 0:
+                # save the running_mean and running_var before updated
                 running_mean = self.running_mean.clone()
                 running_var = self.running_var.clone()
                 output += [F.batch_norm(
-                    input[vt_batch_size*gpu_index:vt_batch_size*(gpu_index+1)],
+                    input[vt_batch_size*vt_gpu_index:vt_batch_size*(vt_gpu_index+1)],
                     # If buffers are not to be tracked, ensure that they won't be updated
                     self.running_mean if not self.training or self.track_running_stats else None,
                     self.running_var if not self.training or self.track_running_stats else None,
                     self.weight, self.bias, bn_training, exponential_average_factor, self.eps)]
             else:
                 output += [F.batch_norm(
-                    input[vt_batch_size*gpu_index:vt_batch_size*(gpu_index+1)],
+                    input[vt_batch_size*vt_gpu_index:vt_batch_size*(vt_gpu_index+1)],
                     # If buffers are not to be tracked, ensure that they won't be updated
                     running_mean if not self.training or self.track_running_stats else None,
                     running_var if not self.training or self.track_running_stats else None,
